4ef62b356d4cff1da71daee5702195e437a006b9
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-26 12:24:56 -0700

    Create Header component

---
+++ src/Header/Header.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'enzyme';
+import Header from './Header';
+
+test('1 + 1 is 2', () => {
+  expect(1+1).toBe(2);
+});
+
+test('Header component should render', () => {
+  expect(<Header/>).toMatchSnapshot();
+});
+
+test('Header component should display the heading todos', () => {
+  expect(render(<Header/>).text()).toEqual('todos');
+})
