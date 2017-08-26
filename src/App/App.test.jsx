aa7b8d7b3736c092eaeba2598ee0084fc5b51507
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-26 12:59:32 -0700

    Add tests for App component

---
+++ src/App/App.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App';
+import Header from '../Header/Header';
+import {shallow} from 'enzyme';
+
+test('renders the App component', () => {
+  expect(shallow(<App/>).exists()).toBe(true);
+});
+
+test('renders all child components', () => {
+  expect(shallow(<App/>).find(Header)).toHaveLength(1);
+  expect(shallow(<App/>).find(AddToDo)).toHaveLength(1);
+  expect(shallow(<App/>).find(ToDos)).toHaveLength(1);
+  expect(shallow(<App/>).find(FilterToDos)).toHaveLength(1);
+});
