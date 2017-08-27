2e491f06b8019dbd906681a863bc7059f2246844
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-27 13:09:58 -0700

    Add propTypes

--- src/ToDos/ToDo/ToDo.jsx
+++ src/ToDos/ToDo/ToDo.jsx
@@ -1,50 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ToDo = (props) => (
   <li>{props.item}</li>
 );
 
+ToDo.propTypes = {
+  item: PropTypes.string.isRequired
+};
+
 export default ToDo;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <li> {props.item} </li>
--- src/ToDos/ToDos.jsx
+++ src/ToDos/ToDos.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ToDo from './ToDo/ToDo';
+import PropTypes from 'prop-types';
 
 const ToDos = (props) => {
   return (
-     <ul>
+    <ul>
       {props.toDoList && props.toDoList.map( (item, i) => (
         <ToDo key={i} item={item} />
       ))}
     </ul>
-  )
+  );
+};
+
+ToDos.propTypes = {
+  toDoList: PropTypes.array.isRequired
 };
 
 export default ToDos;
