7849266e3dfb9fc479ceb6f14962394d08500f21
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-29 09:49:15 -0700

    Set up redux store and redux dev tools chrome extension

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Presentational/App/App';
+import RootReducer from './reducers/rootReducer';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+let store = createStore(
+  RootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  document.getElementById('root')
+);
