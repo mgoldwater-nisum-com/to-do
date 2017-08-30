1c2cc23353cc584781a9dacecb9ddea7fa5ba79d
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-30 10:33:53 -0700

    Change test to look for visibleToDos property from state

--- src/Container/SetUpToggle/SetUpToggle.test.jsx
+++ src/Container/SetUpToggle/SetUpToggle.test.jsx
@@ -4,13 +4,14 @@ import SetUpToggle from './SetUpToggle';
 import {shallow} from 'enzyme';
 
 
-const initialState = {toDoList: {allToDos: {0: {id: 0, completed: false, text: 'foo'}}, toDosInOrder: [0]}};
+const initialState = {visibleToDos: 'All', toDoList: {allToDos: {0: {id: 0, completed: false, text: 'foo'}}, toDosInOrder: [0]}};
 const mockStore = ConfigureStore()(initialState);
 
 test('state is passed to connected ToDos component', () => {
   const wrapper = shallow(<SetUpToggle store={mockStore}/>);
   expect(wrapper.props().toDoList).toEqual(initialState.toDoList.allToDos);
   expect(wrapper.props().toDosInOrder).toEqual(initialState.toDoList.toDosInOrder);
+  expect(wrapper.props().showToDos).toEqual(initialState.visibleToDos);
 });
 
 test('dispatch toggleToDo is passed to connected ToDos component', () => {
