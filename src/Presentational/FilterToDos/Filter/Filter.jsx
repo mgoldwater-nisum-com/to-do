e0e6b4c77fcf2db1aeaba23b52d8977085c36f94
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-30 10:02:13 -0700

    User can toggle visible to do setting upon clicking all, active or completed

--- src/Presentational/FilterToDos/Filter/Filter.jsx
+++ src/Presentational/FilterToDos/Filter/Filter.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Filter = (props) => (
-  <a>{props.children}</a>
+  <a onClick={() => props.onClick(props.children)}>{props.children}</a>
 );
 
 Filter.propTypes = {
-  children: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
 };
 
 export default Filter;
--- src/Presentational/FilterToDos/FilterToDos.jsx
+++ src/Presentational/FilterToDos/FilterToDos.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Filter from './Filter/Filter';
+import PropTypes from 'prop-types';
 
-const FilterToDos = () => (
+const FilterToDos = (props) => (
   <div>
-    <Filter> All </Filter>
-    <Filter> Active </Filter>
-    <Filter> Completed </Filter>
+    <Filter onClick={props.toggleFilter}> All </Filter>
+    <Filter onClick={props.toggleFilter}> Active </Filter>
+    <Filter onClick={props.toggleFilter}> Completed </Filter>
   </div>
 );
+
+FilterToDos.propTypes = {
+  toggleFilter: PropTypes.func.isRequired
+};
 
 export default FilterToDos;
