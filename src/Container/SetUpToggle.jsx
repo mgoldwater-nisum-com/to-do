d3f783873acd895402b342cd46dcf20ce6501deb
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-29 17:21:59 -0700

    Configure SetUpToggle component

---
+++ src/Container/SetUpToggle.jsx
@@ -0,0 +1,19 @@
+import {dispatch} from 'redux';
+import {connect} from 'react-redux';
+import {toggleToDo} from '../actions/actions';
+import ToDos from '../Presentational/ToDos/ToDos';
+
+const mapStateToProps = (state) => {
+  return {
+    toDoList: state.toDoList.allToDos,
+    toDosInOrder: state.toDoList.toDosInOrder
+  };
+};
+
+const mapDispatchToProps = {
+  toggleToDo: (id) => dispatch(toggleToDo(id))
+}
+
+const SetUpToggle = connect(mapStateToProps, mapDispatchToProps)(ToDos);
+
+export default SetUpToggle;
---
+++ src/Container/SetUpToggle.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ConfigureStore from 'redux-mock-store';
+import SetUpToggle from './SetUpToggle';
+import {shallow} from 'enzyme';
+
+
+const initialState = {toDoList: {allToDos: {0: {id: 0, completed: false, text: 'foo'}}, toDosInOrder: [0]}}
+const mockStore = ConfigureStore()(initialState);
+
+test('state is passed to connected ToDos component', () => {
+  const wrapper = shallow(<SetUpToggle store={mockStore}/>);
+  expect(wrapper.props().toDoList).toEqual(initialState.toDoList.allToDos);
+  expect(wrapper.props().toDosInOrder).toEqual(initialState.toDoList.toDosInOrder);
+});
+
+test('dispatch toggleToDo is passed to connected ToDos component', () => {
+  const wrapper = shallow(<SetUpToggle store={mockStore}/>);
+  expect(wrapper.props().toggleToDo).toBeTruthy();
+});
