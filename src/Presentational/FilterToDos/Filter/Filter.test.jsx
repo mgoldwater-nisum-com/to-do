0c4d18afdfca46229251e851410451852e66b950
Nisum <nisum@Admins-MacBook-Pro-5.local>
2017-08-30 10:11:01 -0700

    Add required props to tests

--- src/Presentational/FilterToDos/Filter/Filter.test.jsx
+++ src/Presentational/FilterToDos/Filter/Filter.test.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import Filter from './Filter';
 
+const onClick = jest.fn();
+
 test('Filter component renders', () => (
-  expect(shallow(<Filter> foo </Filter>).exists()).toBe(true)
+  expect(shallow(<Filter onClick={onClick}> foo </Filter>).exists()).toBe(true)
 ));
 
 test('Filter component has the text of its children prop', () => (
-  expect(shallow(<Filter>bar</Filter>).text()).toBe('bar')
+  expect(shallow(<Filter onClick={onClick}>bar</Filter>).text()).toBe('bar')
 ));
--- src/Presentational/FilterToDos/FilterToDos.test.jsx
+++ src/Presentational/FilterToDos/FilterToDos.test.jsx
@@ -3,10 +3,12 @@ import {shallow} from 'enzyme';
 import FilterToDos from './FilterToDos';
 import Filter from './Filter/Filter';
 
+const filterToDos = jest.fn();
+
 test('FilterToDos component renders', () => (
-  expect(shallow(<FilterToDos/>).exists()).toBe(true)
+  expect(shallow(<FilterToDos toggleFilter={filterToDos}/>).exists()).toBe(true)
 ));
 
 test('Filter component is invoked 3 times', () => (
-  expect(shallow(<FilterToDos/>).find(Filter)).toHaveLength(3)
+  expect(shallow(<FilterToDos toggleFilter={filterToDos}/>).find(Filter)).toHaveLength(3)
 ));
