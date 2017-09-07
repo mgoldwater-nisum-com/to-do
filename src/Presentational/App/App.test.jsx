76af32db33a85493b83bf5a05d034e8b9019abe1
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-09-07 13:04:12 -0700

    Refactor App component test

--- src/Presentational/App/App.test.jsx
+++ src/Presentational/App/App.test.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import App from './App';
 import Header from '../Header/Header';
-import AddToDo from '../AddToDo/AddToDo';
-import FilterToDos from '../FilterToDos/FilterToDos';
+import SetUpAddToDo from '../../Container/SetUpAddToDo/SetUpAddToDo';
+import SetUpToggle from '../../Container/SetUpToggle/SetUpToggle';
+import SetUpFilter from '../../Container/SetUpFilter/SetUpFilter';
 import {shallow} from 'enzyme';
 
 test('renders the App component', () => {
   expect(shallow(<App/>).exists()).toBe(true);
 });
 
-xtest('renders all child components', () => {
+test('renders all child components', () => {
   expect(shallow(<App/>).find(Header)).toHaveLength(1);
-  expect(shallow(<App/>).find(AddToDo)).toHaveLength(1);
-  expect(shallow(<App/>).find(ToDos)).toHaveLength(1);
-  expect(shallow(<App/>).find(FilterToDos)).toHaveLength(1);
+  expect(shallow(<App/>).find(SetUpAddToDo)).toHaveLength(1);
+  expect(shallow(<App/>).find(SetUpToggle)).toHaveLength(1);
+  expect(shallow(<App/>).find(SetUpFilter)).toHaveLength(1);
 });
