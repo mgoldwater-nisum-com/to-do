ad22919c6ddcdb7c704ba6e73b771e4e3e9cdc0a
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-09-07 17:22:39 -0700

    Remove determineVisibleToDos from SetUpToggle component

--- src/Container/SetUpToggle/SetUpToggle.jsx
+++ src/Container/SetUpToggle/SetUpToggle.jsx
@@ -1,22 +1,7 @@
 import {connect} from 'react-redux';
 import {toggleCompletion} from '../../actions/actions';
 import ToDos from '../../Presentational/ToDos/ToDos';
-import filter from 'filter-values';
-
-export const determineVisibleToDos = (visibility, toDos) => {
-  switch (visibility) {
-    case 'All':
-      return toDos;
-    case 'Active': 
-      return filter(toDos, (value) => (
-        value.completed === false
-      ));
-    case 'Completed': 
-      return filter(toDos, (value) => (
-        value.completed === true
-      ));
-  }
-};
+import determineVisibleToDos from '../../helpers/determineVisibleToDos.js';
 
 const mapStateToProps = (state) => {
   return {
--- src/Container/SetUpToggle/SetUpToggle.test.jsx
+++ src/Container/SetUpToggle/SetUpToggle.test.jsx
@@ -1,36 +1,7 @@
 import React from 'react';
 import ConfigureStore from 'redux-mock-store';
-import SetUpToggle, {determineVisibleToDos} from './SetUpToggle';
+import SetUpToggle from './SetUpToggle';
 import {shallow} from 'enzyme';
-
-const sampleToDoList = {
-  0: {
-    id: 0,
-    completed: false,
-    text: 'foo'
-  },
-  1: {
-    id: 1,
-    completed: true,
-    text: 'bar'
-  }
-};
-
-test('all toDos are returned when visibleToDos is set to All', () => {
-  expect(determineVisibleToDos('All', sampleToDoList)).toEqual({0: {id: 0, completed: false, text: 'foo'}, 1: {id: 1, completed: true, text: 'bar'}
-  });
-});
-
-test('Uncompleted toDos are returned when visibleToDos is set to Active', () => {
-  expect(determineVisibleToDos('Active', sampleToDoList)).toEqual({0: {id: 0, completed: false, text: 'foo'}
-  });
-});
-
-test('Completed toDos are returned when visibleToDos is set to Completed', () => {
-  expect(determineVisibleToDos('Completed', sampleToDoList)).toEqual({1: {id: 1, completed: true, text: 'bar'}
-  });
-});
-
 
 const initialState = {visibleToDos: 'All', toDoList: {allToDos: {0: {id: 0, completed: false, text: 'foo'}}, toDosInOrder: [0]}};
 const mockStore = ConfigureStore()(initialState);
